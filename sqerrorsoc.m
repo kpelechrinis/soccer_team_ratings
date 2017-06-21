c7a7ef9261b5ef165d54aceb01147f2c92526c41
kpelechrinis <kostas.pelechrinis@gmail.com>
2017-06-20 22:36:04 -0400

    Automatically find the number of teams in the league

--- sqerrorsoc.m
+++ sqerrorsoc.m
@@ -6,6 +6,6 @@ b = 0;
 for i = 1:length(table2array(y(:,1)))
     o = find(teams == home(i));
     d = find(teams == away(i));
-    b = b + (table2array(y(i,4)) - (x(33)*x(o)*x(d+16)))^2;
-    b = b + (table2array(y(i,5)) - (x(34)*x(o+16)*x(d)))^2;
+    b = b + (table2array(y(i,4)) - (x((2*length(teams))+1)*x(o)*x(d+length(teams))))^2;
+    b = b + (table2array(y(i,5)) - (x((2*length(teams))+2)*x(o+length(teams))*x(d)))^2;
 end
